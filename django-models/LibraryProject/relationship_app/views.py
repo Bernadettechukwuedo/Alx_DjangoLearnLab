2652732271da16a0d194d06c5038ec2e248a2c53
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-02-20 14:53:37 +0100

    updated a file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,10 +6,10 @@ from django.views.generic import ListView, DetailView
 # Create your views here.
 def list_books(request):
     book = Book.objects.all()
-    return render(request, "relationship_app\list_books.html", {"books": book})
+    return render(request, "relationship_app/list_books.html", {"books": book})
 
 
 class list_availabilityView(DetailView):
     model = Library
-    template_name = "relationship_app\library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
