5b2795bafde681d5e7a850a1bcb37313ee8a5adc
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-02-20 14:16:35 +0100

    0. Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,15 +5,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
 
 
 class Librarian(models.Model):
@@ -21,3 +30,6 @@ class Librarian(models.Model):
     library = models.OneToOneField(
         Library, related_name="librarians", on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return self.name
