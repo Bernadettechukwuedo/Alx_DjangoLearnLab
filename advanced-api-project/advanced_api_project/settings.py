f4ae293ef8e7747ccab283a2c3841052de8532d5
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-03-14 09:59:01 +0100

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "api",
     "rest_framework",
     "rest_framework.authtoken",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -129,4 +130,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Book, Author
+from rest_framework import generics, filters
+from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 """
@@ -17,6 +19,13 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    # This view allows filtering based on the fields in the book model. it consists of search and order filter
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["title", "publication_year", "author"]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "author__id"]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["title__icontains", "publication_year", "author"]
 
 
 # This view, lists specific books based on the book id parsed and it is read only for unautenticated users
